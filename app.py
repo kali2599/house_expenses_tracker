67fa721d900c7c97c514dec06906a7cc072744dc
kali2599 <davidegent99@gmail.com>
2025-12-30 16:05:43 +0100

    added database_path

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ app = flask.Flask(__name__)
 @app.route('/update')
 def update_value():
 
-    ## CHECK DATABASE HAS BEEN CREATED
-    if not os.path.isfile(f"./{DATABASE}"):
-        print("Database not found. Please run 'sqlite3 data.db < init.sql' and 'sqlite3 data.db < populate.sql' to create the database.")
-        exit(1)
-
+    try:
+        sql_manager = SQLManager(DATABASE)
+        print("[+] Connected to database successfully.")
+    except Exception as e:
+        return f"[!] Could not connect to database. Error: {str(e)}", 500
+    
     # GET PARAMETERS FROM URL
     category = flask.request.args.get('category')
     month = flask.request.args.get('month')
@@ -22,18 +23,22 @@ def update_value():
     ## INPUT VALIDATION
     if not category or not month or not amount:  # mandatory parameters
         return "Error: 'category', 'month', and 'amount' are required parameters.", 400
-    if not amount.isnumeric():
-        return "Error: 'amount' must be a numeric value.", 400
+    
     if month.upper() not in MONTHS_INDEX.values():
         return f"Error: '{month}' is not a valid month.", 400
+    
     if category not in SQL_ATTRIBUTES_EDITABLE:
         return f"Error: '{category}' is not a valid editable attribute.", 400
+    
+    try:
+        amount = float(amount)
+        amount = round(amount, 1)
+    except:
+        return "[!] Invalid value. Please insert a numeric value.", 400
 
-    amount = round(float(amount), 1)
-    month = f"{datetime.now().year}_{month.upper()}"  
 
     ## DATABASE UPDATE
-    sql_manager = SQLManager(DATABASE)
+    month = f"{datetime.now().year}_{month.upper()}"  
     old_value = sql_manager.get_value_by_attrANDmonth(month, category)
     new_value = old_value + amount
     sql_manager.update_value_by_attrANDmonth(month, category, new_value)
@@ -49,4 +54,24 @@ def update_value():
 
 
 
-app.run(HOST='localhost', PORT=8080, debug=True)
+
+if __name__ == "__main__":
+
+    global DATABASE
+    
+    if not os.path.exists("./database_path"):
+        print("[!] Database path file not found. Please insert the database path here and the file will be created automatically.\n")
+        db_path = input(">Insert database path (PATH/data.db): ").strip()
+        print()
+        open("./database_path", "w").write(db_path)
+    
+    DATABASE = open("./database_path", "r").read().strip()
+
+    ## CHECK DATABASE HAS BEEN CREATED
+    if not os.path.isfile(f"{DATABASE}"):
+        print(f"[!]Database not found.\nPlease run 'sqlite3 {DATABASE}< init.sql' and 'sqlite3 {DATABASE}< populate.sql' to create the database.")
+        exit(1)
+
+    print(f"[+] Loading sql database from: {DATABASE}\n")
+
+    app.run(host=HOST, port=PORT, debug=True)
